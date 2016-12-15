42b24368491e6cceb9279e79ce1e223fd869267b
Nathan <nathanperdijk@hotmail.com>
2016-12-15 15:48:02 +0100

    Split up the different response-formatting into three flavors: all (default, returns both input and results in one JsObject), all/debug (separation of input and results in the returned JsObject) and all/resultsonly (only results in the returned JsObject) and provided the different responses with separate endpoints.

--- app/controllers/RestController.scala
+++ app/controllers/RestController.scala
@@ -2,51 +2,75 @@ package controllers
 
 import javax.inject.Inject
 
-import controllers.conversion.{Converter, JsonConversionsProvider}
+import controllers.conversion._
+import controllers.conversion.Converter._
 import org.scalarules.engine.{Context, Fact}
-import play.api.libs.json.{JsError, JsObject, JsSuccess}
-import play.api.mvc.{Action, Controller}
+import play.api.libs.json.{JsError, JsObject, JsSuccess, JsValue}
+import play.api.mvc.{Action, Controller, Request}
 import services.{DerivationsService, GlossariesService, JsonConversionMapsService}
 
 // scalastyle:off public.methods.have.type
 
 class RestController @Inject() (derivationsService: DerivationsService, glossariesService: GlossariesService, jsonConversionMapsService: JsonConversionMapsService) extends Controller {
 
-  val jsonConversionMap: JsonConversionsProvider = jsonConversionMapsService.mergedJsonConversionMap
-
   /**
     * Provides a REST endpoint for triggering all derivations in the target project. Any fact definitions available in the target project's glossaries
     * can be provided in the JSON request body like so:
     * {
-    *   "factOfTypeString": "factText",
-    *   "factOfTypeBedrag": 234,
-    *   "factOfTypeBigDecimal": 234,
-    *   "factOfTypePercentage": 234
+    * "factOfTypeString": "factText",
+    * "factOfTypeBedrag": 234,
+    * "factOfTypeBigDecimal": 234,
+    * "factOfTypePercentage": 234
     * }
     *
     * @return A JsonObject containing either:
     *         - A list of JsErrors, containing complete error information concerning failed conversions from json to context (if multiple errors occur, you receive information on all of them)
-    *         - A JsObject containing three JsObjects:
-    *               - "inputFacts", which contains all the fact information submitted by the caller
-    *               - "outputFacts", which contains all the fact information derived by applying the inputFacts to the rules
-    *               - "allFacts", which contains the combined information of "inputFacts" and "outputFacts"
+    *         - A JsObject containing one JsObject: "facts", which contains the combined information of "input" and "results"
     */
   def runAll = Action(parse.json) { request =>
+    run(request, DefaultResponseJsObject)
+  }
+
+  /**
+    * As #runAll except:
+    *
+    * @return A JsonObject containing either:
+    *         - A list of JsErrors, containing complete error information concerning failed conversions from json to context (if multiple errors occur, you receive information on all of them)
+    *         - A JsObject containing two JsObject: "input" and "results", which contains only the information of "results"
+    */
+  def runAllDebug = Action(parse.json) { request =>
+    run(request, DebugResponseJsObject)
+  }
+
+  /**
+    * As #runAll except:
+    *
+    * @return A JsonObject containing either:
+    *         - A list of JsErrors, containing complete error information concerning failed conversions from json to context (if multiple errors occur, you receive information on all of them)
+    *         - A JsObject containing one JsObject: "results", which contains only the information of "results"
+    */
+  def runAllResultsOnly = Action(parse.json) { request =>
+    run(request, ResultsOnlyResponseJsObject)
+  }
+
+
+  val jsonConversionMap: JsonConversionsProvider = jsonConversionMapsService.mergedJsonConversionMap
+
+  private def run(request: Request[JsValue], jsonResponseProvider: ResponseJsObject) = {
     val (initialContextFragments: List[JsSuccess[Context]], conversionErrors: List[JsError]) =
-      Converter.convertToIndividualContext(request.body, glossariesService.mergedGlossaries, jsonConversionMap)
+      convertToIndividualContext(request.body, glossariesService.mergedGlossaries, jsonConversionMap)
 
-    if(conversionErrors != List.empty)
-      BadRequest(JsError.toJson(conversionErrors.reduceLeft(_ ++ _)))
-    else {
-      val initialContext: Context = initialContextFragments.foldLeft(Map.empty[Fact[Any], Any])((acc, jsSuccess) => acc ++ jsSuccess.get)
-      val resultContext: Context = RulesRunner.run(initialContext, derivationsService.derivations)
+    if (conversionErrors != List.empty) BadRequest( processConversionErrors(conversionErrors) )
+    else Ok( processConvertedContext(initialContextFragments, jsonResponseProvider) )
+  }
 
-      Ok(JsObject(
-          Map("inputFacts" -> Converter.contextToJson(initialContext, jsonConversionMap)) ++
-          Map("outputFacts" -> Converter.contextToJson(resultContext -- initialContext.keys, jsonConversionMap)) ++
-          Map("allFacts" -> Converter.contextToJson(resultContext, jsonConversionMap)))
-      )
-    }
+  private def processConversionErrors(conversionErrors: List[JsError]): JsObject = JsError.toJson(conversionErrors.reduceLeft(_ ++ _))
+
+  private def processConvertedContext(initialContextFragments: List[JsSuccess[Context]], jsonResponse: ResponseJsObject): JsObject = {
+    val initialContext: Context = initialContextFragments.foldLeft(Map.empty[Fact[Any], Any])((acc, jsSuccess) => acc ++ jsSuccess.get)
+    val resultContext: Context = RulesRunner.run(initialContext, derivationsService.derivations)
+
+    jsonResponse.toJson(initialContext = initialContext, resultContext = resultContext, jsonConversionMap)
   }
 
 }
