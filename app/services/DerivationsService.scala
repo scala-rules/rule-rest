76d79bc85fb1c26850fd1f3a2df0d215897f1eea
Nathan <nathanperdijk@hotmail.com>
2016-12-21 14:53:30 +0100

    Cleaned up the DerivationsService, because it was unnecessarily complex: Removed the val derivations which we don't actually use. Removing that allowed for the simplification of the rest of the code into a more concise flatMap that requires no helper ckass. Also removed the lazy aspects of vals: we will always use these vals, so better take the performance  hit when starting up the service rather than during the first call.

--- app/services/DerivationsService.scala
+++ app/services/DerivationsService.scala
@@ -9,22 +9,16 @@ import play.api.{Configuration, Logger}
 @Singleton
 class DerivationsService @Inject() (configuration: Configuration, jarLoaderService: JarLoaderService) {
 
-  val derivationsWithMeta: List[DerivationHolder[_ <: Berekening]] = jarLoaderService.jars.flatMap(jarEntry => {
-    jarEntry._2.derivations.map( d => new DerivationHolder(d.berekeningen, d.getClass) )
-  }).toList
+  private val elementBerekeningClass = classOf[ElementBerekening[Any, Any]]
 
-  val derivations: List[Derivation] = derivationsWithMeta.flatMap( _.derivations )
+  val topLevelDerivations: List[Derivation] = {
+    val topLevelDerivationsWithMeta = jarLoaderService.jars.flatMap(
+      jarEntry => { jarEntry._2.derivations.filterNot( d => elementBerekeningClass.isAssignableFrom( d.getClass ))}
+    ).toList
 
-  lazy val elementBerekeningClass = classOf[ElementBerekening[Any, Any]]
-  lazy val topLevelDerivations: List[Derivation] = {
-    val topLevelDerivationsWithMeta = derivationsWithMeta.filter( d => !elementBerekeningClass.isAssignableFrom( d.originalClass ) )
+    Logger.info(s"Detected the following top-level derivations: [${topLevelDerivationsWithMeta.map( _.getClass.getName ).mkString(", ")}]")
 
-    Logger.info(s"Detected the following top-level derivations: [${topLevelDerivationsWithMeta.map( _.originalClass.getName ).mkString(", ")}]")
-
-    topLevelDerivationsWithMeta.flatMap( _.derivations )
+    topLevelDerivationsWithMeta.flatMap( _.berekeningen )
   }
 
 }
-
-class DerivationHolder[T <: Berekening](val derivations: List[Derivation], val originalClass: Class[T])
-
